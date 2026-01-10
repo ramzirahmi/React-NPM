169a4950165c9bc693357ad1ed53abec62e2a10a
Ramzi rahmi <ramzirahmi@gmail.com>
2026-01-10 22:25:43 +0100

    Output If Condition

--- src/Components/Listitem.js
+++ src/Components/Listitem.js
@@ -1,14 +1,17 @@
 function Listitem({ ville }) {
   return (
     <>
-      {" "}
-      {ville.map((itemvile, index) => (
-        <div key={index}>
-          <p>{itemvile.name}</p>
-          <p>{itemvile.surface} km</p>
-          <p>----------------------</p>
-        </div>
-      ))}
+      {ville.map(
+        (itemville, index) =>
+          // If Condition
+          itemville.surface > 50 && (
+            <div key={index}>
+              <p>{itemville.name}</p>
+              <p>{itemville.surface} km</p>
+              <p>----------------------</p>
+            </div>
+          )
+      )}
     </>
   );
 }
